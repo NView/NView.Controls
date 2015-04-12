368b119a8bd7165c7df8361b495c0ce72a9b769f
James Montemagno <james.montemagno@gmail.com>
2015-04-11 23:36:37 -0700

    Initial Commit

---
+++ NView.Controls/NView.Controls/Button.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace NView.Controls
+{
+	/// <summary>
+	/// Cross platform Button for NView.
+	/// </summary>
+	public class Button : IView
+	{
+		/// <summary>
+		/// Gets or sets the title.
+		/// </summary>
+		/// <value>The title.</value>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="NView.Controls.Button"/> is enabled.
+		/// </summary>
+		/// <value><c>true</c> if enabled; otherwise, <c>false</c>.</value>
+		public bool Enabled { get; set; }
+
+		/// <summary>
+		/// Event that occurs when button is clicked.
+		/// </summary>
+		public event EventHandler Clicked;
+
+		#region IView implementation
+
+		/// <summary>
+		/// Binds the IView to a native view.
+		/// </summary>
+		/// <returns>A disposable view</returns>
+		/// <param name="nativeView">Native view to bind with.</param>
+		public IDisposable BindToNative (object nativeView)
+		{
+			Helpers.ThrowNotImplementedException ();
+		}
+
+		/// <summary>
+		/// Gets the type of the preferred native control.
+		/// </summary>
+		/// <value>The type of the preferred native.</value>
+		public Type PreferredNativeType {
+			get {
+				Helpers.ThrowNotImplementedException ();
+			}
+		}
+
+		#endregion
+	}
+}
+
---
+++ NView.Controls/NView.Controls/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NView.Controls
+{
+	internal static class Helpers
+	{
+		const string NotImplemented = "This functionality is not implemented in the portable version of this assembly.  You should reference the NView.Controls NuGet package from your application in order to reference the platform-specific implementation.";
+
+		public static void ThrowNotImplementedException()
+		{
+			throw new NotImplementedException (NotImplemented);
+		}
+	}
+}
+
