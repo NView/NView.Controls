e15ad45b1bf36989cfac91366a4678551a7c33d8
Frank A. Krueger <fak@praeclarum.org>
2015-04-19 20:48:10 -0700

    Stub out the Android Stack

---
+++ NView.Controls.Android/Controls/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Android.Widget;
+
+namespace NView.Controls
+{
+	/// <summary>
+	/// Cross platform Stack layout for NView. Stacks can be horizontal or vertical.
+	/// </summary>
+	public class Stack : IView
+	{
+		/// <summary>
+		/// The orientation of the <see cref="Stack"/>.
+		/// </summary>
+		public StackOrientation Orientation { get; set; }
+
+		/// <summary>
+		/// Add a child view to this stack with the given layout.
+		/// </summary>
+		/// <param name="child">The child view.</param>
+		/// <param name="layout">The layout to associate with the child.</param>
+		public void AddChild (IView child, StackLayout layout)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#region IView implementation
+
+		/// <inheritdoc/>
+		public void BindToNative (object nativeView, BindOptions options = BindOptions.None)
+		{
+			throw new NotImplementedException ();
+		}
+
+		/// <inheritdoc/>
+		public void UnbindFromNative ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		/// <inheritdoc/>
+		public Type PreferredNativeType {
+			get {
+				return typeof(LinearLayout);
+			}
+		}
+
+		#endregion
+	}
+}
+
